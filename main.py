2c009e52830f3dc1dbca6817d4386324743d6526
Ubuntu <ubuntu@ip-10-0-0-11.excloud.local>
2026-07-04 15:25:13 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from server-test!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def root():
+    return {"message": "Hello Server"}
+
+@app.get("/api")
+def api():
+    return {"message": "Hello from API"}
